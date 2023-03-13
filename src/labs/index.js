9b01f6fd8ce3512fe8fef9fa3c194a6d499dcc4a
Maolin Zhang <maolinzq1@gmail.com>
2023-03-13 15:21:01 -0400

    A7 lab completion

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,12 +1,26 @@
-import Assignment6 from "./a6";
 import Nav from "../nav";
+import Assignment6 from "./a6";
+import Assignment7 from "./a7";
+import {Routes, Route} from "react-router";
+
 function Labs() {
     return (
         <div>
             <Nav/>
-            <Assignment6/>
+            <strong>Note: </strong>
+            <em>
+                The Tuiter link in Nav and the Tuiter link in the old
+                homepage are NOT the same. The old homepage only includes the
+                Tuiter as of A5
+            </em>
+            <Routes>
+                <Route index
+                       element={<Assignment6/>}/>
+                <Route path="/a7"
+                       element={<Assignment7/>}/>
+            </Routes>
         </div>
     );
 }
-export default Labs;
 
+export default Labs;
--- src/nav.js
+++ src/nav.js
@@ -4,7 +4,8 @@ function Nav() {
     return (
         <div>
             <Link to="/">Labs</Link> |
-            <Link to="/hello">Hello</Link> |
+            <Link to="/">Assignment 6</Link> |
+            <Link to="/a7">Assignment 7</Link> |
             <Link to="/tuiter">Tuiter</Link> |
             <Link to="/old-home">Old Home</Link>
             {/* Added extra link to keep make old home page links available */}
